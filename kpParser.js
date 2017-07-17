a416a5f774a0b48bbcd871005c2a255ca3d8f8b5
ilgizkasymov <ilgizkasymov@gmail.com>
2017-07-17 16:28:13 +0600

    refactoring kp parser code

--- kpParser.js
+++ kpParser.js
@@ -1,5 +1,4 @@
 var request = require('request');
-var check = true;
 var xpath = require('xpath'),
     dom = require('xmldom').DOMParser;
 var sh = require('cheerio');
@@ -7,14 +6,6 @@ var fs = require('fs');
 var superagent = require('superagent');
 var client = require('redis').createClient('redis://h:pd4c104be5ed6b00951dd5c0f8c7461f66790fc55dde2d58612b10a98bb2e5a20@ec2-34-230-117-175.compute-1.amazonaws.com:28789');
 
-function changeCheckVariable(){
-    check = !check
-}
-var last_values = 0;
-var last_news = 0;
-client.get('kp_news', function (error, value) {
-    last_news = value
-});
 function getAuthToken(callback){
         var data = {
             url: 'https://api.namba1.co/users/auth',
@@ -35,154 +26,155 @@ function getAuthToken(callback){
         })
 }
 
-function getArticleTheme(url, callback){
-        var data = {
-            url: url,
-            method: 'GET'
-        };
-        request(data, function (error, req, body) {
-            if (error || req.statusCode === 404){
-                callback('error')
-            }else {
-                var doc = new dom().parseFromString(body);
-                var head = xpath.select('//*[@id="bodyArticleJS"]/header', doc).toString();
-                var $ = sh.load(head);
-                callback($('h1').text());
-            }
-        })
-}
-
-function downloadImage(url){
-
-}
 
 
-function getArticleImage(url, callback){
-    var data = {
-        url: url,
-        method: 'GET'
-    };
-    request(data, function (error, req, body) {
-        if (error || req.statusCode === 404){
-            callback('error')
-        }else{
-            var doc = new dom().parseFromString(body);
-            var imageHtml = xpath.select('//*[@id="bodyArticleJS"]/header/div[4]/div/img', doc).toString();
-            var $ = sh.load(imageHtml);
-            try {
-                var imgUrl = 'http:' + $('img')[0].attribs.src;
-            }catch (error){
-                callback('none');
-                return;
-            }
-            if (imgUrl){
-                var value = url.split('/').slice(-2).slice(0);
-                request(imgUrl).pipe(fs.createWriteStream('./' + 'kp' +  value + '.jpg')).on('finish', function (error, req) {
-                    if (error){
-                        console.log(error)
-                    }
-                    superagent.post('https://files.namba1.co').attach('file', './' + 'kp' +  value + '.jpg').end(function(err, req) {
-                        fs.unlink('./' + 'kp' +  value + '.jpg', function (error, value) {});
-                        callback(req.body.file)
-                    });
-                });
-            }
-        }
-    })
-}
-
-function getArticleBody(url, callback) {
-    var data = {
-        url: url,
-        method: 'GET'
-    };
-    request(data, function (error, req, body) {
-        if (error || req.statusCode === 404){
-            callback('error')
-        }else{
-            var doc = new dom().parseFromString(body);
-            var articleBodyHtml = xpath.select('//*[@id="hypercontext"]', doc).toString();
-            var $ = sh.load(articleBodyHtml);
-            var text = '';
-            $('p').slice(0).each(function (i, element) {
-               text += $(this).text()
-            });
-            callback(text)
-        }
-    })
-}
 
 function postArticle(data, callback){
-    request(data, function (error, req, body) {
+    var dataForSend = {
+        url: 'https://api.namba1.co/groups/' + 1142 +'/post',
+        method: 'POST',
+        body: {
+            content: data.title + '\r\n' + data.text,
+            comment_enabled: 1
+        },
+        headers: {
+            'X-Namba-Auth-Token': data.sendToken
+        },
+        json: true
+    };
+    if (data.imgToken === 'none'){
+    }else{
+        dataForSend.body['attachments'] = [{
+            type: 'media/image',
+            content: data.imgToken
+        }]
+    }
+    request(dataForSend, function (error, req, body) {
         callback(req.statusCode)
     })
 }
 
-function baseLogic(callback) {
-    getAuthToken(function (sendToken) {
-        client.get('kp_news', function (error, value) {
-            var url = 'http://www.kp.kg/online/news/' + value + '/';
-            getArticleTheme(url, function (theme) {
-                if(theme === 'error'){
-                    last_values += 1;
-                    client.set('kp_news', parseInt(value) + 1);
-                    if(last_values === 5){
-                       client.set('kp_news', last_news);
-                       changeCheckVariable();
-                       callback(check);
-                       return;
-                   }else{
-                       callback(true);
-                       return;
-                   }
-                }else{
-                    getArticleBody(url, function (text) {
-                        getArticleImage(url, function (token) {
-                            var data = {
-                                url: 'https://api.namba1.co/groups/' + 1137 +'/post',
-                                method: 'POST',
-                                body: {
-                                    content: theme + '\r\n' + text,
-                                    comment_enabled: 1,
-                                },
-                                headers: {
-                                    'X-Namba-Auth-Token': sendToken
-                                },
-                                json: true
-                            };
-                            if (token === 'none'){
-                            }else{
-                                data.body['attachments'] = [{
-                                    type: 'media/image',
-                                    content: token
-                                }]
-                            }
-                            postArticle(data, function (statusCode) {
-                                console.log(statusCode);
-                                client.set('kp_news', parseInt(value) + 2, function (error, value) {
-                                    callback(check)
-                                });
-                            })
-                        })
-                    });
-                }
-            })
+function getArticleImage(doc, callback){
+    var imageHtml = xpath.select('//*[@id="bodyArticleJS"]/header/div[4]/div/img', doc).toString();
+    var $ = sh.load(imageHtml);
+    try {
+        var imgUrl = 'http:' + $('img')[0].attribs.src;
+    }catch (error){
+        callback('none');
+        return;
+    }
+    if (imgUrl){
+        var value = Math.random();
+        request(imgUrl).pipe(fs.createWriteStream('./' + 'kp' +  value + '.jpg')).on('finish', function (error, req) {
+            if (error){
+                console.log(error)
+            }
+            superagent.post('https://files.namba1.co').attach('file', './' + 'kp' +  value + '.jpg').end(function(err, req) {
+                fs.unlink('./' + 'kp' +  value + '.jpg', function (error, value) {});
+                callback(req.body.file)
+            });
         });
-
-    });
+    }
 }
 
-function AsyncWhileLoop() {
-    baseLogic(function (check) {
-        if (check){
-            AsyncWhileLoop()
-        }else{
-            console.log('end');
-            process.exit()
+function getArticleBody(doc, callback) {
+    var articleBodyHtml = xpath.select('//*[@id="hypercontext"]', doc).toString();
+    var $ = sh.load(articleBodyHtml);
+    var text = '';
+    $('p').slice(0).each(function (i, element) {
+        text += $(this).text()
+    });
+    callback(text)
+}
+
+
+function getArticleThemeTextImgToken(url, callback){
+    var data = {
+        url: url,
+        method: 'GET'
+    };
+    request(data, function (error, req, body) {
+        if (error || req.statusCode === 404){
+            callback('error')
+        }else {
+            var doc = new dom().parseFromString(body);
+            var head = xpath.select('//*[@id="bodyArticleJS"]/header', doc).toString();
+            var $ = sh.load(head);
+            var title = $('h1').text();
+            getArticleBody(doc, function (text) {
+                getArticleImage(doc, function (token) {
+                    callback(title, text, token)
+                })
+            })
         }
-    });
+    })
 }
 
+function returnArticleForSend(url, sendToken, callback){
+    getArticleThemeTextImgToken(url, function (title, text, imgToken) {
+        var data = {
+            title: title,
+            text: text,
+            imgToken: imgToken,
+            sendToken: sendToken
+        };
+        postArticle(data, function (statusCode) {
+            callback(statusCode)
+        })
+    })
 
+}
 
-AsyncWhileLoop();
+function baseLogic(id, sendToken, callback) {
+        var url = 'http://www.kp.kg/online/news/' + id + '/';
+        returnArticleForSend(url, sendToken, function (statusCode) {
+            callback(statusCode)
+        });
+}
+
+function getUrl(){
+    var data = {
+        url: 'http://www.kp.kg/',
+        method: 'GET'
+    };
+    request(data, function (error, req, body) {
+        var doc = new dom().parseFromString(body);
+        var leftsiteBar = xpath.select('//*[@id="newsRegionJS"]', doc).toString();
+        var $ = sh.load(leftsiteBar);
+        var ids = $('div').children('article').map(function(i, elem){
+            return [$(this).attr('data-news-id')]
+        }).get().reverse();
+        var afterLoopList = [];
+        var counter = 0;
+        client.get('kp_news', function (error, value) {
+            getAuthToken(function (sendToken) {
+                ids.forEach(function (key) {
+                    counter ++;
+                    if (value < key &&  value !== key){
+                        baseLogic(key, sendToken, function (statusCode) {
+                            afterLoopList.push(key);
+                            if (counter === ids.length){
+                                var check = afterLoopList[0];
+                                var checkList = 0;
+                                afterLoopList.forEach(function(key){
+                                    checkList++;
+                                    if (key > check){
+                                        check = key
+                                    }
+                                    if (checkList === afterLoopList.length){
+                                        client.set('kp_news', check, function (error) {
+                                            console.log('STOOP')
+                                        })
+                                    }
+                                })
+                            }
+                        });
+                    }else{
+                        console.log('Not Page')
+                    }
+                });
+            });
+        });
+    });
+}
+getUrl();
