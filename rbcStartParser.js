9ca7032bbaf7826091506d2d32a424c2aad1edcc
ilgizkasymov <ilgizkasymov@gmail.com>
2017-07-21 12:50:27 +0600

    review rbc parser

--- rbcStartParser.js
+++ rbcStartParser.js
@@ -13,138 +13,132 @@ function getArticleUrls(){
         };
         request(data, function(error, req, body){
             if (error){
-                rejected(error)
+                rejected(error);
             }
             var doc = new dom().parseFromString(body);
             var leftSideBarHtml = xpath.select('/html/body/div[8]/div/div[2]/div[1]/div/div/div/div/div[2]/div', doc).toString();
             var $ = sh.load(leftSideBarHtml);
             var urls = $('a').map(function (i, elem) {
-                return [$(this).attr('href')]
+                return [$(this).attr('href')];
             }).get();
             resolve(urls);
-        })
+        });
     });
 }
 
 function getArticleImages(doc) {
     return new Promise(function (resolve, rejected) {
-        var imageHtml = xpath.select('/html/body/div[8]', doc).toString();
-        var $ = sh.load(imageHtml);
-        var imgUrls = '';
+        let imageHtml = xpath.select('/html/body/div[8]', doc).toString();
+        let $ = sh.load(imageHtml);
+        let imgUrls = '';
         $('div').each(function(i, elem){
             if($(this).attr('class') === 'article__main-image__link'){
                 imgUrls += $(this).children('img')[0].attribs.src;
             }
         });
         if(imgUrls){
-            resolve(imgUrls)
+            resolve(imgUrls);
         }else{
-            rejected('not Pictures')
+            rejected(false);
         }
     });
 }
 
 function getArticleTheme(doc){
     return new Promise(function (resolve, rejected) {
-        var titleHtml = xpath.select('/html/body/div[8]', doc).toString();
-        var $ = sh.load(titleHtml);
-        var title = $('div').filter('.article__header__title').children('span').text();
-        resolve(title)
-    })
+        let titleHtml = xpath.select('/html/body/div[8]', doc).toString();
+        let $ = sh.load(titleHtml);
+        let title = $('div').filter('.article__header__title').children('span').text();
+        resolve(title);
+    });
 
 }
 
 function getArticleBody(doc) {
     return new Promise(function (resolve, rejected) {
-        var bodyHtml = xpath.select("/html/body/div[8]", doc).toString();
-        var $ = sh.load(bodyHtml);
-        var text = '';
+        let bodyHtml = xpath.select("/html/body/div[8]", doc).toString();
+        let $ = sh.load(bodyHtml);
+        let text = '';
         $('div').each(function(i, elem){
             if($(this).attr('class') === 'article__text'){
                 $('p').slice(0).each(function (i, element) {
-                    text += $(this).text().replace(methods.regex, '') + '\r\n\r\n'
+                    text += $(this).text().replace(methods.regex, '') + '\r\n\r\n';
                 });
             }
         });
-        resolve(text)
-    })
+        resolve(text);
+    });
 }
 function sendArticle(data){
-    return new Promise(function (resolve, rejected) {
-        var dataForSend = {
-            url: 'https://api.namba1.co/groups/' + 1146 +'/post',
-            method: 'POST',
-            body: {
-                content: data.title + '\r\n\r\n' + data.text,
-                comment_enabled: 1
-            },
-            headers: {
-                'X-Namba-Auth-Token': data.sendToken
-            },
-            json: true
-        };
-        if(data.img){
-            dataForSend.body['attachments'] = [{
-                type: 'media/image',
-                content: data.img
-            }]
+    let dataForSend = {
+        url: 'https://api.namba1.co/groups/' + 1146 +'/post',
+        method: 'POST',
+        body: {
+            content: data.title + '\r\n\r\n' + data.text,
+            comment_enabled: 1
+        },
+        headers: {
+            'X-Namba-Auth-Token': data.sendToken
+        },
+        json: true
+    };
+    if(data.img){
+        dataForSend.body['attachments'] = [{
+            type: 'media/image',
+            content: data.img
+        }];
+    }
+    request(dataForSend, function (error, req, body) {
+        if(error){
+            return error;
         }
-        request(dataForSend, function (error, req, body) {
-            if(error){
-                rejected(error)
-            }
-            resolve(req.statusCode)
-        })
+        return req.statusCode;
     });
 }
 
 function getDocForParser(url){
-    return new Promise(function(resolve, rejected){
-        var data = {
+    return new Promise((resolve, rejected) =>{
+        let data = {
             url: url,
             method: 'GET'
         };
         request(data, function(error, req, body){
             if(!error){
-                resolve(new dom().parseFromString(body))
+                resolve(new dom().parseFromString(body));
             }
             rejected(error);
-        })
+        });
     });
 }
 
-function getArticleThemeBodyImageToken(url, sendToken) {
-    return new Promise(function(resolve, rejected){
-        getDocForParser(url)
-            .then(function(doc){
-                getArticleTheme(doc)
-                    .then(function (theme) {
-                        getArticleBody(doc)
-                            .then(function (body) {
-                                var data = {
-                                    title: theme,
-                                    text: body,
-                                    sendToken: sendToken
-                                };
-                                getArticleImages(doc)
-                                    .then(function (imgUrls) {
-                                        methods.saveImageAndReturnToken(imgUrls, function (token) {
-                                            data['img'] = token;
-                                            resolve(data)
-                                        })
-                                    })
-                                    .catch(function (error) {
-                                        resolve(data)
-                                    });
-                            })
-                    })
-            });
-    });
+
+function getArticleThemeBodyImageToken(args) {
+    getDocForParser(args.urls)
+        .then(doc => {
+            Promise.all([getArticleTheme(doc), getArticleBody(doc), getArticleImages(doc)])
+                .then(results =>{
+                    let data = {
+                        title: results[0],
+                        body: results[1],
+                        sendToken: args.token
+                    };
+                    let img = data[2];
+                    if(img){
+                        methods.saveImageAndReturnToken(img, function (token) {
+                           data['img'] = token;
+                        });
+                    }
+                    console.log(data)
+                    // console.log(sendArticle(data));
+                });
+        });
 }
+
 
 Array.prototype.random = function(){
   return this[Math.floor((Math.random()*this.length))];
 };
+
 
 function divideListAndReturnUrl(urls){
     return new Promise(function(resolve, rejected){
@@ -153,45 +147,36 @@ function divideListAndReturnUrl(urls){
                 var before = urls.indexOf(value);
                 var divideList = urls.slice(0, before);
                 if(divideList.length > 0){
-                    resolve(divideList.random())
+                    resolve(divideList.random());
                 }
-                rejected()
+                rejected();
             }
-            rejected(error)
-        })
+            rejected(error);
+        });
     });
 }
 
+
 function callFunction(){
     getArticleUrls()
-        .then(function (urls) {
-            divideListAndReturnUrl(urls)
-                .then(function(randomUrl){
-                    client.set('rbc_last_news', randomUrl, function (error) {
-                        methods.getAuthToken(function (sendToken) {
-                            getArticleThemeBodyImageToken(randomUrl, sendToken)
-                                .then(function(data){
-                                    sendArticle(data)
-                                        .then(function (statusCode) {
-                                            console.log(statusCode)
-                                        });
-                                })
-                                .catch(function (data) {
-                                    sendArticle(data)
-                                        .then(function (statusCode) {
-                                            console.log(statusCode)
-                                        });
-                                })
-                        });
-                    });
-                })
-                .catch(function(error){
-                    console.log(error)
+        .then(urls =>{
+            return divideListAndReturnUrl(urls);
+        })
+        .then(randomUrl =>{
+            return new Promise((resolve, rejected) =>{
+                methods.getAuthToken((token)=>{
+                    resolve({token: token, urls: randomUrl});
                 });
+            });
         })
-        .catch(function (error) {
-            console.log(error)
+        .then(data => {
+            // console.log(data);
+            getArticleThemeBodyImageToken(data);
         })
+        .catch(error => {
+            console.log(error + 'sdf');
+        });
 }
+
 
 callFunction();
