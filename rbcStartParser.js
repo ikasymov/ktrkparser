f181f8c5019f91b6ef187c0dc74379110e6dbb29
ilgizkasymov <ilgizkasymov@gmail.com>
2017-07-24 16:21:44 +0600

    code review

--- rbcStartParser.js
+++ rbcStartParser.js
@@ -1,102 +1,57 @@
-var request = require('request');
-var sh = require('cheerio');
-var xpath = require('xpath');
-var dom = require('xmldom').DOMParser;
-var methods = require('./methods');
-var client = require('redis').createClient('redis://h:pd4c104be5ed6b00951dd5c0f8c7461f66790fc55dde2d58612b10a98bb2e5a20@ec2-34-230-117-175.compute-1.amazonaws.com:28789');
+let request = require('request');
+let sh = require('cheerio');
+let xpath = require('xpath');
+let dom = require('xmldom').DOMParser;
+let methods = require('./methods');
+let superagent = require('superagent');
+let fs = require('fs');
+let config = require('./config');
+let Parser = require('./parser');
+let client = require('redis').createClient('redis://h:pd4c104be5ed6b00951dd5c0f8c7461f66790fc55dde2d58612b10a98bb2e5a20@ec2-34-230-117-175.compute-1.amazonaws.com:28789');
 
-function getArticleUrls(){
-    return new Promise(function (resolve, rejected) {
-        var data = {
-            url: 'http://www.rbc.ru/',
-            method: 'GET'
-        };
-        request(data, function(error, req, body){
-            if (error){
-                rejected(error);
-            }
-            var doc = new dom().parseFromString(body);
-            var leftSideBarHtml = xpath.select('/html/body/div[8]/div/div[2]/div[1]/div/div/div/div/div[2]/div', doc).toString();
-            var $ = sh.load(leftSideBarHtml);
-            var urls = $('a').map(function (i, elem) {
-                return [$(this).attr('href')];
-            }).get();
-            resolve(urls);
-        });
-    });
+Array.prototype.random = function(){
+    return this[Math.floor((Math.random()*this.length))];
+};
+
+
+function RBCParser(config){
+    Parser.apply(this, arguments);
 }
 
-function getArticleImages(doc) {
-    return new Promise(function (resolve, rejected) {
-        let imageHtml = xpath.select('/html/body/div[8]', doc).toString();
-        let $ = sh.load(imageHtml);
-        let imgUrls = '';
-        $('div').each(function(i, elem){
-            if($(this).attr('class') === 'article__main-image__link'){
-                imgUrls += $(this).children('img')[0].attribs.src;
-            }
-        });
-        if(imgUrls){
-            resolve(imgUrls);
-        }else{
-            rejected(false);
-        }
-    });
-}
 
-function getArticleTheme(doc){
-    return new Promise(function (resolve, rejected) {
-        let titleHtml = xpath.select('/html/body/div[8]', doc).toString();
-        let $ = sh.load(titleHtml);
-        let title = $('div').filter('.article__header__title').children('span').text();
-        resolve(title);
-    });
+RBCParser.prototype = Object.create(Parser.prototype);
+RBCParser.prototype.constructor = RBCParser;
 
-}
 
-function getArticleBody(doc) {
-    return new Promise(function (resolve, rejected) {
-        let bodyHtml = xpath.select("/html/body/div[8]", doc).toString();
-        let $ = sh.load(bodyHtml);
-        let text = '';
-        $('div').each(function(i, elem){
-            if($(this).attr('class') === 'article__text'){
-                $('p').slice(0).each(function (i, element) {
-                    text += $(this).text().replace(methods.regex, '') + '\r\n\r\n';
-                });
-            }
-        });
-        resolve(text);
-    });
-}
-function sendArticle(data){
-    let dataForSend = {
-        url: 'https://api.namba1.co/groups/' + 1146 +'/post',
-        method: 'POST',
-        body: {
-            content: data.title + '\r\n\r\n' + data.text,
-            comment_enabled: 1
-        },
-        headers: {
-            'X-Namba-Auth-Token': data.sendToken
-        },
-        json: true
-    };
-    if(data.img){
-        dataForSend.body['attachments'] = [{
-            type: 'media/image',
-            content: data.img
-        }];
+RBCParser.prototype.start = async function () {
+    this._randomUrl = await this._generateRandom();
+    client.set(this.dataName, await this._randomUrl);
+    this._sendToken = await this.generateToken();
+    if(await this._randomUrl && await this._sendToken){
+        let resultCode = await this._sendArticle();
+        console.log(resultCode)
     }
-    request(dataForSend, function (error, req, body) {
-        if(error){
-            return error;
-        }
-        return req.statusCode;
-    });
-}
+};
 
-function getDocForParser(url){
+RBCParser.prototype._generateRandom = async function () {
+    let urls = await this._urls();
+    return new Promise((resolve, rejected)=>{
+        client.get(this.dataName, function(error, value){
+            if(!error){
+                let before = urls.indexOf(value);
+                let divideList = urls.slice(0, before);
+                if(divideList.length > 0){
+                    resolve(divideList.random())
+                }else{
+                    rejected(Error('Not random List'));
+                }
+            }
+        });
+    });
+};
+
+RBCParser.prototype._doc = async function(){
+    let url = await this._randomUrl;
     return new Promise((resolve, rejected) =>{
         let data = {
             url: url,
@@ -109,76 +64,69 @@ function getDocForParser(url){
             rejected(error);
         });
     });
-}
+};
 
-
-function getArticleThemeBodyImageToken(args) {
-    getDocForParser(args.urls)
-        .then(doc => {
-            Promise.all([getArticleTheme(doc), getArticleBody(doc), getArticleImages(doc)])
-                .then(results =>{
-                    let data = {
-                        title: results[0],
-                        body: results[1],
-                        sendToken: args.token
-                    };
-                    let img = data[2];
-                    if(img){
-                        methods.saveImageAndReturnToken(img, function (token) {
-                           data['img'] = token;
-                        });
-                    }
-                    console.log(data)
-                    // console.log(sendArticle(data));
-                });
+RBCParser.prototype._urls = async function(){
+    let parseUrl = this.parseUrl;
+    return new Promise(function (resolve, rejected) {
+        let data = {
+            url: parseUrl,
+            method: 'GET'
+        };
+        request(data, function(error, req, body){
+            if (error){
+                rejected(error);
+            }
+            let doc = new dom().parseFromString(body);
+            let leftSideBarHtml = xpath.select('/html/body/div[8]/div/div[2]/div[1]/div/div/div/div/div[2]/div', doc).toString();
+            let $ = sh.load(leftSideBarHtml);
+            let urls = $('a').map(function (i, elem) {
+                return [$(this).attr('href')];
+            }).get();
+            resolve(urls);
         });
-}
-
-
-Array.prototype.random = function(){
-  return this[Math.floor((Math.random()*this.length))];
+    });
 };
 
 
-function divideListAndReturnUrl(urls){
-    return new Promise(function(resolve, rejected){
-        client.get('rbc_last_news', function(error, value){
-            if(!error){
-                var before = urls.indexOf(value);
-                var divideList = urls.slice(0, before);
-                if(divideList.length > 0){
-                    resolve(divideList.random());
-                }
-                rejected();
-            }
-            rejected(error);
-        });
+RBCParser.prototype.getArticleImages = async function(){
+    let doc = await this._doc();
+    let imageHtml = xpath.select('/html/body/div[8]', doc).toString();
+    let $ = sh.load(imageHtml);
+    let imgUrls = '';
+    $('div').each(function(i, elem){
+        if($(this).attr('class') === 'article__main-image__link'){
+            imgUrls += $(this).children('img')[0].attribs.src;
+        }
     });
-}
+    if(imgUrls){
+        return await this._saveImageByUrl(imgUrls)
+    }else{
+        return false;
+    }
+};
 
+RBCParser.prototype.getArticleTheme = async function(){
+    let doc = await this._doc();
+    let titleHtml = xpath.select('/html/body/div[8]', doc).toString();
+    let $ = sh.load(titleHtml);
+    return $('div').filter('.article__header__title').children('span').text();
+};
 
-function callFunction(){
-    getArticleUrls()
-        .then(urls =>{
-            return divideListAndReturnUrl(urls);
-        })
-        .then(randomUrl =>{
-            return new Promise((resolve, rejected) =>{
-                methods.getAuthToken((token)=>{
-                    resolve({token: token, urls: randomUrl});
-                });
+RBCParser.prototype.getArticleBody = async function(){
+    let doc = await this._doc();
+    let bodyHtml = xpath.select("/html/body/div[8]", doc).toString();
+    let $ = sh.load(bodyHtml);
+    let text = '';
+    $('div').each(function(i, elem){
+        if($(this).attr('class') === 'article__text'){
+            $('p').slice(0).each(function (i, element) {
+                text += $(this).text().replace(methods.regex, '') + '\r\n\r\n';
             });
-        })
-        .then(data => {
-            // console.log(data);
-            getArticleThemeBodyImageToken(data);
-        })
-        .catch(error => {
-            console.log(error + 'sdf');
-        });
-}
+        }
+    });
+    return text;
+};
 
-
-callFunction();
-
-//testing
+let parser = new RBCParser(config);
+parser.start();
