44606be5b23e7c7e430f0ced8dbef26cee71242f
ilgizkasymov <ilgizkasymov@gmail.com>
2017-07-25 17:46:46 +0600

    remove config from gitignore

---
+++ config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    rbk: {
+        group: 1150,
+        nambaone: 'https://api.namba1.co',
+        parserUrl: 'https://rbc.ru/',
+        dataName: 'test_rbk_list',
+        user: '996121121121',
+        passport: 'password112'
+    },
+    ktrk: {
+        group: 1150,
+        nambaone: 'https://api.namba1.co',
+        parserUrl: 'http://www.ktrk.kg/',
+        dataName2: 'test_ktrk_check',
+        dataName: 'test_ktrk_list',
+        user: '996121121121',
+        passport: 'password112'
+    }
+};
+
