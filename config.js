d79b8e3a3bbb8083c5c431a28e343897ad80730f
ilgizkasymov <ilgizkasymov@gmail.com>
2017-07-25 18:03:22 +0600

    Revert "update config"
    This reverts commit 9db958bd45144239695e7e1a33ca203497dc3aa2.

--- config.js
+++ config.js
@@ -8,24 +8,13 @@ module.exports = {
         passport: 'password112'
     },
     ktrk: {
-        ktrkRu: {
-            group: 1144,
-            nambaone: 'https://api.namba1.co',
-            parserUrl: 'http://www.ktrk.kg/',
-            dataName2: 'test_ktrk_check',
-            dataName: 'test_ktrk_list',
-            user: '996121121121',
-            passport: 'password112'
-        },
-        ktrkKG: {
-            group: 1143,
-            nambaone: 'https://api.namba1.co',
-            parserUrl: 'http://www.ktrk.kg/',
-            dataName2: 'test_ktrk_check',
-            dataName: 'test_ktrk_list',
-            user: '996121121121',
-            passport: 'password112'
-        }
+        group: 1150,
+        nambaone: 'https://api.namba1.co',
+        parserUrl: 'http://www.ktrk.kg/',
+        dataName2: 'test_ktrk_check',
+        dataName: 'test_ktrk_list',
+        user: '996121121121',
+        passport: 'password112'
     }
 };
 
